121ea95051d8dadd1bcaefed50a614ca2b921874
Daehwan Cho <daehwancho@Daehwanui-Macmini.local>
2020-02-19 02:43:15 +0900

    bug fixed.. uri error

--- graphql/db.js
+++ graphql/db.js
@@ -1,15 +1,25 @@
 import fetch from "node-fetch";
 
-const API_URL = "https://yts.mx/api/v2/list_movies.json";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
 export const getMovies = (limit, rating) => {
     let REQUEST_URL = API_URL;
+    // console.log(API_URL);
     if (limit > 0) {
         REQUEST_URL += `limit=${limit}`;
     }
     if (rating > 0) {
         REQUEST_URL += `&minimum_rating=${rating}`;
     }
+    // if (limit > 0) {
+    //     REQUEST_URL += "limit=";
+    //     REQUEST_URL += limit;
+    // }
+    // if (rating > 0) {
+    //     REQUEST_URL += "&minimum_rating=";
+    //     REQUEST_URL += rating;
+    // }
+    // console.log(REQUEST_URL);
     return fetch(REQUEST_URL)
         .then(res => res.json())
         .then(json => json.data.movies);
