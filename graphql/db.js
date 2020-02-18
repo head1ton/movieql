e866f9cc1dd84e48a39435ff1742b765641f1302
Daehwan Cho <daehwancho@Daehwanui-Macmini.local>
2020-02-19 00:22:31 +0900

    creating first mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,43 +1,59 @@
-export const people = [
+export const movies = [
     {
         id: 0,
         name: "Head1ton",
-        age: 20,
-        gender: "female"
+        score: 20
     },
     {
         id: 1,
         name: "Head2ton",
-        age: 30,
-        gender: "female"
+        score: 30
     },
     {
         id: 2,
         name: "Head3ton",
-        age: 40,
-        gender: "female"
+        score: 40
     },
     {
         id: 3,
         name: "Head4ton",
-        age: 50,
-        gender: "female"
+        score: 50
     },
     {
         id: 4,
         name: "Head5ton",
-        age: 60,
-        gender: "female"
+        score: 60
     },
     {
         id: 5,
         name: "Head6ton",
-        age: 70,
-        gender: "female"
+        score: 70
     }
 ];
 
 export const getById = id => {
     const filteredPeople = people.filter(person => person.id === id);
     return filteredPeople[0];
+};
+
+export const getMovies = () => movies;
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanedMovies.length) {
+        movie = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: movies.length + 1, // `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 };
